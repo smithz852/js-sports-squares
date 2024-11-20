92263a550bf70b27ced24f3b98756625b074aede
Zach Smith <zachsmith852@gmail.com>
2024-11-19 18:27:18 -0800

    adjust stat 400 code

--- public/js/game.js
+++ public/js/game.js
@@ -306,7 +306,7 @@ document.querySelector('.X-box').classList.add('.xText');
   console.log("Select Game", data);
   console.log('global time', globalTime)
 
-  if (data.statusCode === 400 && data.length < 1) {
+  if (data.statusCode === 400 || data.length < 1) {
     setTimeout(() => {
       let fillerTimer = localStorage.getItem('GT')
       console.log('no games available')

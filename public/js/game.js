79597594d82dfe3542452a08d70e1e6c6bb30249
Zach Smith <zachsmith852@gmail.com>
2025-02-09 16:41:21 -0800

    update time

--- public/js/game.js
+++ public/js/game.js
@@ -67,7 +67,7 @@ function displayTimer(game) {
   } else {
     timerDescr.textContent = 'Refresh Time'
     //console.log('normal fetch')
-   requestUrl = `/api/nflApiFetch/selectedGame/${scoreID}/timer/${600}`
+   requestUrl = `/api/nflApiFetch/selectedGame/${scoreID}/timer/${601}`
   }
 
 
